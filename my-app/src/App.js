bbee336126b3ebceeae90b53bc7ba3f17d904ac0
Pavithra Vineeth <paviatwork26@gmail.com>
2023-06-16 10:40:51 +0000

    my-todo-app

---
+++ my-app/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+
+  const [taskName, setTaskName] = useState();
+
+  const [tasks, setTasks] = useState([]);
+
+  const updateTaskName = (value) => {
+    setTaskName(value)
+  }
+
+  const createTask = () => {
+    console.log(taskName)
+    if(!taskName){
+      return
+    }
+    setTasks([...tasks, taskName])
+    clearTask()
+  }
+
+  const clearTask = () => {  
+    setTaskName("")
+  }
+  
+
+
+
+  return (
+    <div>
+      <label>Enter task name </label>
+      
+      <input type="text" value={taskName} onChange={(e) => {
+        updateTaskName(e.target.value)
+      }}></input>
+
+      <button type="submit" onClick={createTask}>Create Task</button>
+      <button type="clear" onClick={clearTask}>clear Task</button>
+
+      <div>
+        <h3>List of Tasks {tasks.length}</h3>
+
+        {
+          tasks.map((t, index) => {
+            return <div key={index}>{t}</div>
+          })
+        }
+  
+      </div>
+    </div>
+  );
+}
+
+export default App;
